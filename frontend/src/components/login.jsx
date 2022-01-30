567090ba6cd64cecee74d02cebebf5006d6e258b
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-01-30 22:26:16 +0100

    feat(frontend/components/Login.js)
    First API request created with functioning form.

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import StyledLogin, {LoginButton, TextInput} from "./styles/Login.styled";
+import Axios from "axios";
 
-const LogIn = () => {
+//const [loginStatus, setLoginStatus] = useState(false);
+
+function LogIn() {
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
     const SignIn = (e) => {
         e.preventDefault();
+        Axios.post("http://localhost:4000/login", {
+            username: data.email,
+            password: data.password
+        })
+            .then(
+                res => {
+                    console.log(res.data);
+                })
     }
-    return(
+
+    function handleChange(e) {
+        const newData = {...data};
+        newData[e.target.id] = e.target.value;
+        setData(newData);
+    }
+
+    return (
         <StyledLogin>
             <form onSubmit={SignIn}>
                 <div>
@@ -13,9 +35,10 @@ const LogIn = () => {
                     <sub>Created by Peter Seres</sub>
                 </div>
                 <div>
-                    <TextInput type="text" placeholder={"Email"}/>
+                    <TextInput onChange={(e) => handleChange(e)} id="email" type="text" placeholder={"Email"}/>
                     <br/>
-                    <TextInput type="password" placeholder={"Password"}/>
+                    <TextInput onChange={(e) => handleChange(e)} id="password" type="password"
+                               placeholder={"Password"}/>
                 </div>
                 <LoginButton>
                     <p>Log in</p>
