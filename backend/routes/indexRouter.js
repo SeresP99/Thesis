a13689eee806b1cd3a8ee4dcb7a6921875627800
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-02-04 23:26:38 +0100

    fix(routes/indexRouter): program logic fix in login

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -8,15 +8,16 @@ const {verify} = require("jsonwebtoken");
 router.use(express.json());
 
 const verifyJWT = (req, res, next) => {
-    const token = req.headers["X-access-Token"];
-
+    const token = req.headers["x-access-token"];
     if (!token) {
         res.send("We need a token, please give it to us next time! - server")
     } else {
         jwt.verify(token, process.env.COOKIE_SECRET, (err, decoded) => {
             if (err) {
+                console.log("bad token");
                 res.json({auth: false, message: "You failed to authenticate! - server"})
             } else {
+                console.log("correct token");
                 req.userId = decoded.id;
                 next();
             }
@@ -25,24 +26,30 @@ const verifyJWT = (req, res, next) => {
 }
 
 router.get("/", (req, res) => {
-    res.send("hi home");
+    res.send("server home");
 });
 
 router.post("/login", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const user = await user_model.getUserId(username, password);
-    console.log("User's credentials: " + user);
-    const id = user.id;
-    const token = jwt.sign({id}, process.env.COOKIE_SECRET, {expiresIn: '60s'});
+    console.log(req.body);
 
-    res.json({auth: true, token: token, user: user});
+    try {
+        const user = await user_model.getUserId(username, password);
+        console.log("User's credentials: " + JSON.stringify(user));
+        const id = user.id;
+        const token = jwt.sign({id}, process.env.COOKIE_SECRET, {expiresIn: '60s'});
+        res.json({auth: true, token: token, user: user});
+    } catch (e) {
+        res.json({auth: false, message:"Wrong email or password!"})
+    }
+
 });
 
 
-router.get("/profile", verifyJWT, (req, res) => {
-    res.send("You are authenticated!");
+router.get("/checkAuth", verifyJWT, (req, res) => {
+    res.json({auth: true});
 });
 
 
