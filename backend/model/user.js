c9088f28e562aed52e508b81b981d588cb3b6721
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-02-04 23:25:33 +0100

    fix(model/user): failure to find user handled

--- backend/model/user.js
+++ backend/model/user.js
@@ -3,10 +3,15 @@ const pool = require("./db");
 
 class user_model {
     static async getUserId(username, password) {
-        const credentials = await pool.query("SELECT id FROM users WHERE username = $1 AND password = $2", [username, password]);
-        console.log(credentials.rows[0]);
-        return credentials.rows[0];
+        try {
+            const credentials = await pool.query("SELECT id FROM users WHERE username = $1 AND password = $2",
+                [username, password]);
+            return credentials.rows[0];
+        } catch (e) {
+            console.error(e.message);
+        }
     }
+
 }
 
 module.exports = user_model;
