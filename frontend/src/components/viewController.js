2dad94014d4f9fdecd6302536fe4e1084a72a95b
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-02-04 23:30:39 +0100

    feat(viewController): default route set

--- frontend/src/components/viewController.js
+++ frontend/src/components/viewController.js
@@ -1,12 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import SignUp from "./signup";
 import LogIn from "./login";
+import ProfilePage from "./profilePage"
+
 
 const Views = () => {
     return (
         <Routes>
-            <Route path="/" element={<LogIn />}/>
-            <Route path="/register" element={<SignUp />} />
+            <Route path="/" element={<LogIn/>}/>
+            <Route path="/profile" element={<ProfilePage/>}/>
+            <Route path="/register" element={<SignUp/>}/>
+            <Route path="*" element={<LogIn/>}/>
         </Routes>
     );
 };
