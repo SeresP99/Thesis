852a32dc96fe615cb1d1c7365e8befc6834a1d26
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-03-06 21:04:56 +0100

    feat(indexRouter): add account creation API

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -42,9 +42,38 @@ router.post("/login", async (req, res) => {
         const token = jwt.sign({id}, process.env.COOKIE_SECRET, {expiresIn: '60s'});
         res.json({auth: true, token: token, user: user});
     } catch (e) {
-        res.json({auth: false, message:"Wrong email or password!"})
+        res.json({auth: false, message: "Wrong email or password!"})
     }
 
+});
+
+
+router.post("/register", async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const isEmailAvailable = await user_model.checkIfEmailAvailable(email);
+        const isUsernameAvailable = await user_model.checkIfUsernameAvailable(username);
+
+        if (!isEmailAvailable && !isUsernameAvailable)
+            res.json({success: false, message: "Email and username are not available!"})
+        else if (!isEmailAvailable)
+            res.json({success: false, message: "Email is not available!"})
+        else if (!isUsernameAvailable)
+            res.json({success: false, message: "Username is not available!"})
+        else
+            try {
+                const creation = await user_model.createUser(username, email, password);
+                res.json({success: true, message:"Account created successfully, you can log in soon!"})
+            } catch (e) {
+                res.json({success: false, message: "Internal server error."})
+            }
+    } catch (e) {
+        console.error(e.message);
+        res.json({success: false, message: "Internal server error."})
+    }
 });
 
 
