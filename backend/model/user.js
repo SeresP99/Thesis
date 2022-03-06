f0c03c94661ba91f13220be17569caf0b90dee2a
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-03-06 21:03:52 +0100

    feat(user_model): add create user
    and check for taken username and email

--- backend/model/user.js
+++ backend/model/user.js
@@ -7,6 +7,21 @@ class user_model {
                 [username, password]);
             return credentials.rows[0];
     }
+
+    static async createUser(username, password, email) {
+            const creation = await pool.query("INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
+                [username, email, password]);
+            return creation.rows[0];
+    }
+
+    static async checkIfUsernameAvailable(username){
+            const checkUsername = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            return checkUsername.rows[0] === undefined;
+    }
+
+    static async checkIfEmailAvailable(email){
+        const checkEmail = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        return checkEmail.rows[0] === undefined;
     }
 
 }
