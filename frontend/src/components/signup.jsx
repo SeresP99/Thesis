9b2fe72ecf963ff679923124cd303c924dfee56d
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-03-06 21:07:00 +0100

    feat(signup page): add value change event

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import StyledSignup, {SignupButton, TextInput} from "./styles/Signup.styled"
+import Axios from "axios";
 
 const Signup = () => {
-    const SignIn = (e) => {
+
+    const [data, setData] = useState({
+        email: "",
+        username: "",
+        password: ""
+    });
+
+    const CreateAccount = (e) => {
         e.preventDefault();
+
+        Axios.post("http://localhost:4000/register", {
+            email: data.email,
+            username: data.username,
+            password: data.password
+        }).then(
+            res => {
+                console.log(res.data);
+                if (res.data.success === true)
+                    return (<p>Successful sign up!</p>)
+                else
+                    return (<p>Unsuccessful sign up!</p>)
+            }
+        )
+    };
+
+    function handleChange(e) {
+        const newData = {...data};
+        newData[e.target.id] = e.target.value;
+        setData(newData);
     }
+
     return (
         <StyledSignup>
-            <form onSubmit={SignIn}>
+            <form onSubmit={CreateAccount}>
                 <div>
                     <h1>Plum</h1>
                     <sub>Created by Peter Seres</sub>
                 </div>
                 <div>
-                    <TextInput type="text" placeholder={"Email"}/>
+                    <TextInput onChange={(e) => handleChange(e)} id="email" type="text" placeholder={"Email"}/>
                     <br/>
-                    <TextInput type="text" placeholder={"Full Name"}/>
+                    <TextInput onChange={(e) => handleChange(e)} id="username" type="text" placeholder={"Username"}/>
                     <br/>
-                    <TextInput type="password" placeholder={"Password"}/>
+                    <TextInput onChange={(e) => handleChange(e)} id="password" type="password"
+                               placeholder={"Password"}/>
                 </div>
                 <SignupButton>
                     <p>Create account</p>
