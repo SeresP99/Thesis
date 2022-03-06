c8398a7fb562884fef4d91adf2d4a6740a808489
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-03-06 21:02:54 +0100

    feat(user_model): remove unnecessary try catch block in GetUserId

--- backend/model/user.js
+++ backend/model/user.js
@@ -3,13 +3,10 @@ const pool = require("./db");
 
 class user_model {
     static async getUserId(username, password) {
-        try {
             const credentials = await pool.query("SELECT id FROM users WHERE username = $1 AND password = $2",
                 [username, password]);
             return credentials.rows[0];
-        } catch (e) {
-            console.error(e.message);
-        }
+    }
     }
 
 }
