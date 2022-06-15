e9047e7db3fc68cb3ea9a014f372544ccf213c3b
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-06-15 12:17:35 +0200

    feat(backend/model): poll detail query

--- backend/model/users_polls.js
+++ backend/model/users_polls.js
@@ -7,11 +7,18 @@ class users_polls_model {
         return query.rows[0];
     }
 
-    static async getCreatedPolls(userid){
+    static async getCreatedPolls(userId){
         const query = await pool.query(
             "SELECT users.username as participant, polls.title, polls.id as poll_id FROM users JOIN relationship_polls_users ON users.id = relationship_polls_users.user_id JOIN polls ON relationship_polls_users.poll_id = polls.id WHERE user_id = $1",
-            [userid])
+            [userId])
         return query.rows;
+    }
+
+    static async getPollDetails(pollId){
+        const query = await pool.query(
+            "SELECT * FROM polls WHERE id = $1", [pollId]
+        )
+        return query.rows[0];
     }
 }
 
