cca8c52e92bed4e308bd72bf766cffc61be075fb
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-06-15 12:21:28 +0200

    feat(frontend/view controller): added detailed poll view route

--- frontend/src/components/viewController.js
+++ frontend/src/components/viewController.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import SignUp from "./signup";
 import LogIn from "./login";
 import ProfilePage from "./profilePage"
+import PollDetails from "./pollDetails";
 
 
 const Views = () => {
@@ -9,6 +10,7 @@ const Views = () => {
         <Routes>
             <Route path="/" element={<LogIn/>}/>
             <Route path="/profile" element={<ProfilePage/>}/>
+            <Route path="/profile/poll" element={<PollDetails/>}/>
             <Route path="/register" element={<SignUp/>}/>
             <Route path="*" element={<LogIn/>}/>
         </Routes>
