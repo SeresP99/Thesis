4729035a7550b21160ba99695d432d6ad60019c8
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-06-15 12:20:23 +0200

    feat(frontend/poll list): navigation to detailed poll view

--- frontend/src/components/ProfilePageComponents/PollList.js
+++ frontend/src/components/ProfilePageComponents/PollList.js
@@ -4,22 +4,27 @@ import {Scrollbars} from "react-custom-scrollbars-2";
 import CustomScrollbars from "../global/Scrollbar";
 import {AddButton, PollListElement, PollButtonPanel, PollNameTag} from "./PollListStyle";
 import Axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const PollList = () => {
+
+    const navigate = useNavigate();
 
     const [pollList, setPollList] = useState([]);
 
     const getPollList = () => {
         Axios.get("http://localhost:4000/getCreatedPolls", {
             headers: {
-                'x-access-token': localStorage.getItem('token')
+                'x-access-token': localStorage.getItem('token'),
             }
-            ,
         }).then(
             res => {
                 setPollList(res.data.polls);
-                console.log(res.data.polls);
             })
+    }
+
+    function goToPollDetails(key) {
+        navigate("/profile/poll", {state: {pollId: key}})
     }
 
     useEffect(() => {
@@ -31,7 +36,7 @@ const PollList = () => {
         return <PollListElement>
             <PollNameTag>{props.title}</PollNameTag>
             <PollButtonPanel>
-                <AddButton Key="props.id">Add</AddButton>
+                <AddButton onClick={() => goToPollDetails(props.id)}>View</AddButton>
             </PollButtonPanel>
         </PollListElement>
     }
@@ -42,7 +47,7 @@ const PollList = () => {
     return (
         <div style={{width: '80%'}}>
             <CustomScrollbars style={{borderRadius: '5px', height: 300}}>
-                {pollList.map((poll) => <Poll key={poll.poll_id} title={poll.title}/>)}
+                {pollList.map((poll) => <Poll key={poll.poll_id} title={poll.title} id={poll.poll_id}/>)}
             </CustomScrollbars>
         </div>
     )
