8c3dd5b810240420f8d1defe50fc12f3fb0fd95f
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-06-24 13:25:43 +0200

    feat(backend/model): fix table naming

--- backend/model/users_polls.js
+++ backend/model/users_polls.js
@@ -3,13 +3,13 @@ const pool = require("./db");
 class users_polls_model {
     static async getAllRelationships(){
         const query = await pool.query(
-            "SELECT users.username as participant, polls.title FROM users JOIN relationship_polls_users ON users.id = relationship_polls_users.user_id JOIN polls ON relationship_polls_users.poll_id = polls.id")
+            "SELECT users.username as participant, polls.title FROM users JOIN participants_polls_relationship ON users.id = participants_polls_relationship.user_id JOIN polls ON participants_polls_relationship.poll_id = polls.id")
         return query.rows[0];
     }
 
     static async getCreatedPolls(userId){
         const query = await pool.query(
-            "SELECT users.username as participant, polls.title, polls.id as poll_id FROM users JOIN relationship_polls_users ON users.id = relationship_polls_users.user_id JOIN polls ON relationship_polls_users.poll_id = polls.id WHERE user_id = $1",
+            "SELECT users.username as participant, polls.title, polls.id as poll_id FROM users JOIN participants_polls_relationship ON users.id = participants_polls_relationship.user_id JOIN polls ON participants_polls_relationship.poll_id = polls.id WHERE user_id = $1",
             [userId])
         return query.rows;
     }
