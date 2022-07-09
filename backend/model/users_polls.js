f0e8b5d829deb41ea074cea4647605da81216bb5
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-07-09 20:12:20 +0200

    fix(backend/model/users_polls): code formatting

--- backend/model/users_polls.js
+++ backend/model/users_polls.js
@@ -1,41 +1,48 @@
 const pool = require("./db");
 
 class users_polls_model {
-    static async getAllRelationships(){
+    static async getAllRelationships() {
         const query = await pool.query(
             "SELECT users.username as participant, polls.title FROM users JOIN participants_polls_relationship ON users.id = participants_polls_relationship.user_id JOIN polls ON participants_polls_relationship.poll_id = polls.id")
         return query.rows[0];
     }
 
-    static async getParticipatingPolls(userId){
+    static async getParticipatingPolls(userId) {
         const query = await pool.query(
             "SELECT users.username as participant, polls.title, polls.id as poll_id FROM users JOIN participants_polls_relationship ON users.id = participants_polls_relationship.user_id JOIN polls ON participants_polls_relationship.poll_id = polls.id WHERE user_id = $1",
             [userId])
         return query.rows;
     }
 
-    static async getPollDetails(pollId){
+    static async getPollDetails(pollId) {
         const query = await pool.query(
             "SELECT * FROM polls WHERE id = $1", [pollId]
         )
         return query.rows[0];
     }
 
-    static async createPoll(poll, authorId){
+    static async getCreatedPolls(authorId) {
+        const query = await pool.query(
+            "SELECT * FROM polls WHERE author_id = $1", [authorId]
+        )
+        return query.rows;
+    }
+
+    static async createPoll(poll, authorId) {
         const query = await pool.query(
             "INSERT INTO public.polls (title, description, author_id, opening_date, closure_date) VALUES ($1, $2, $3, $4, $5)", [poll.title, poll.description, authorId, poll.startDate, poll.endDate]
         )
         return query.rows[0];
     }
 
-    static async getPollOptions(pollId){
+    static async getPollOptions(pollId) {
         const query = await pool.query(
             "SELECT * FROM public.poll_options WHERE poll_id = $1", [pollId]
         );
         return query.rows;
     }
 
-    static async addPollOption(pollId, title, description){
+    static async addPollOption(pollId, title, description) {
         const query = await pool.query(
             "INSERT INTO public.poll_options (title, description, poll_id) VALUES ($1, $2, $3)", [title, description, pollId]
         );
@@ -43,7 +50,7 @@ class users_polls_model {
         return query.rows[0];
     }
 
-    static async updatePollOption(id, title, description){
+    static async updatePollOption(id, title, description) {
         const query = await pool.query(
             "UPDATE poll_options SET title = $1, description = $2 WHERE id = $3",
             [title, description, id]
@@ -51,7 +58,7 @@ class users_polls_model {
         return query.rows[0];
     };
 
-    static async deletePollOption(id){
+    static async deletePollOption(id) {
         const query = await pool.query(
             "DELETE FROM poll_options WHERE id = $1",
             [id]
