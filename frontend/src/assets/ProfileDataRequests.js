040d8fc82eeaf27c54fae4d14b0da78b29d38343
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-07-19 10:57:20 +0200

    feat(frontend/assets): add getAllUserData

---
+++ frontend/src/assets/ProfileDataRequests.js
@@ -0,0 +1,14 @@
+import Axios from "axios";
+
+const backend = process.env.REACT_APP_SERVER;
+
+export function getAllUserData() {
+    return Axios.get(backend+ "/getUserProfile", {
+        headers: {
+            'x-access-token': localStorage.getItem("token"),
+        },
+    }).then(res => {
+            return res.data.profile;
+        }
+    );
+}
