a2247d8f9fdc67cbd1b409922ab4401629371b28
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2022-07-20 11:39:38 +0200

    fix(frontend/edit vote option page): bloat code & appearance

--- frontend/src/pages/EditVoteOptions.jsx
+++ frontend/src/pages/EditVoteOptions.jsx
@@ -1,4 +1,9 @@
-import {BackFromEditingButton, ButtonRow} from "../components/styles/EditVoteOptionsStyle";
+import {
+    BackFromEditingButton,
+    ButtonRow,
+    FlexboxToScrollThrough, OptionListContainer,
+    Scrollbar
+} from "../components/styles/EditVoteOptionsStyle";
 import CustomScrollbars from "../components/global/Scrollbar"
 import Axios from "axios";
 import {useLocation} from "react-router-dom";
@@ -35,32 +40,17 @@ function EditVoteOptions() {
         navigate("/dashboard");
     }
 
-    const scrollStyle = {
-        borderRadius: '5px',
-        height: 380,
-        width: "80%",
-        minWidth: "300px",
-        marginTop: "5px"
-    }
-    const divStyle = {
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-evenly",
-        alignContent: "space-around",
-        height: 290
-    }
-
     return (
         <BasicPage>
             <BasicContentCard>
-
-                <CustomScrollbars style={scrollStyle}>
-                    <div style={divStyle}>
-                        {optionList.map((option) => <VoteOption key={option.id} title={option.title} id={option.id}
-                                                                description={option.description}/>)}
-                    </div>
-                </CustomScrollbars>
+                <OptionListContainer>
+                    <Scrollbar>
+                        <FlexboxToScrollThrough>
+                            {optionList.map((option) => <VoteOption key={option.id} title={option.title} id={option.id}
+                                                                    description={option.description}/>)}
+                        </FlexboxToScrollThrough>
+                    </Scrollbar>
+                </OptionListContainer>
                 <ButtonRow>
                     <BackFromEditingButton onClick={NavToDash}>Back</BackFromEditingButton>
                     <CreatePopup/>
